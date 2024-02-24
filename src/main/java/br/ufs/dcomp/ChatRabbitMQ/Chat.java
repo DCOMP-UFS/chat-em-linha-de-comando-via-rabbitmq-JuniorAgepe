9f03797a7671520da4bae99daee2a07f9d2083a9
José_Arnaldo <52730658+JuniorAgepe@users.noreply.github.com>
2024-02-23 22:14:29 -0300

    Update Chat.java

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -12,7 +12,13 @@ import java.time.LocalDate;
 
 import java.time.LocalTime;
 
+import java.io.File;
+
+import java.nio.file.*;
+
 import java.io.*;
+
+import com.google.protobuf.ByteString;
 
 import com.google.protobuf.util.JsonFormat;
 
@@ -20,14 +26,43 @@ import java.time.format.DateTimeFormatter;
 
 public class Chat {
 
+  
+  
   private static String horarioAtual(){
-    LocalDate data = LocalDate.now();
     LocalTime horaAtual = LocalTime.now();
-    DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     DateTimeFormatter formatoHoraMinutos = DateTimeFormatter.ofPattern("HH:mm");
     
-    return "(" + data.format(formatoData) + " às " + horaAtual.format(formatoHoraMinutos) + ")";
+    return horaAtual.format(formatoHoraMinutos);
+  }
+  private static String dataAtual(){
+    LocalDate data = LocalDate.now();
+    DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    
+    return data.format(formatoData);
+  }
+  
+  private static Void DecideEnvio(String QUEUE_Send, Mensagem.MensagemUsuario.Builder builderMensagem, Channel channel, String QUEUE_NAME){
+    try {
+      if(QUEUE_Send.substring(0,1).equals("@")){
+          
+          builderMensagem.setGrupo("");
+          Mensagem.MensagemUsuario mensagemUsuario = builderMensagem.build();
+          byte[] buffer = mensagemUsuario.toByteArray();
+          channel.basicPublish("", QUEUE_Send, null,  buffer); 
+        }
+      else{
+        
+        builderMensagem.setGrupo(QUEUE_Send);
+        Mensagem.MensagemUsuario mensagemUsuario = builderMensagem.build();
+        byte[] buffer = mensagemUsuario.toByteArray();
+        channel.basicPublish(QUEUE_Send.substring(1), QUEUE_NAME , null, buffer); 
+      }
     }
+    catch(IOException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
   
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
@@ -37,6 +72,7 @@ public class Chat {
     factory.setVirtualHost("/");   
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
+    final Channel channel2 = connection.createChannel();
     String Usuario = "";
     String Remetente = "";
     
@@ -51,29 +87,56 @@ public class Chat {
     final String nomeUsuario = QUEUE_NAME;
     QUEUE_NAME = "@" + QUEUE_NAME; 
     channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+    channel2.queueDeclare(QUEUE_NAME+"Up", false,   false,     false,       null);
     Consumer consumer = new DefaultConsumer(channel) {
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
         
         //String message = new String(body, "UTF-8");
         byte[] message = body;
         Mensagem.MensagemUsuario mensagemUsuario = Mensagem.MensagemUsuario.parseFrom(message);
+        Mensagem.Conteudo conteudoMensagem = mensagemUsuario.getConteudo();
         System.out.println();
-        if(mensagemUsuario.getGrupo().equals("")){
-          System.out.println(mensagemUsuario.getHora() + " " + mensagemUsuario.getNome() + " diz: " + mensagemUsuario.getMensagem() );
-        }
-        else{
-          if(!mensagemUsuario.getNome().equals("@" + nomeUsuario)){
-            System.out.println(mensagemUsuario.getHora() + " " + mensagemUsuario.getGrupo() + mensagemUsuario.getNome() + " diz: " + mensagemUsuario.getMensagem());
-          }
-          
+        if(!mensagemUsuario.getEmissor().equals("@" + nomeUsuario)){
+          System.out.println(mensagemUsuario.getData() + " às " + mensagemUsuario.getHora() + " " + mensagemUsuario.getGrupo() + mensagemUsuario.getEmissor() + " diz: " + conteudoMensagem.getCorpo().toString("UTF-8"));
         }
         System.out.print(">>>");
                         //(deliveryTag,               multiple);
         //channel.basicAck(envelope.getDeliveryTag(), false);
       }
     };
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
-    System.out.print( QUEUE_Send + ">>>");
+    Consumer consumer2 = new DefaultConsumer(channel2) {
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
+        
+        //String message = new String(body, "UTF-8");
+        byte[] message = body;
+        Mensagem.MensagemUsuario mensagemUsuario = Mensagem.MensagemUsuario.parseFrom(message);
+        Mensagem.Conteudo conteudoMensagem = mensagemUsuario.getConteudo();
+        try {
+          File file = new File("/home/ubuntu/environment/chat/uploads/"+conteudoMensagem.getNome());
+          if (file.createNewFile()) {
+              System.out.println("Arquivo criado com sucesso!");
+          } else {
+              System.out.println("O arquivo já existe.");
+          }
+          ByteString conteudo = conteudoMensagem.getCorpo();
+          byte[] bytes = conteudo.toByteArray();
+          ByteString byteString = ByteString.copyFrom(bytes);
+          FileOutputStream outputStream = new FileOutputStream(file, true);
+          outputStream.write(byteString.toByteArray());
+          outputStream.close();
+          } catch (IOException e) {
+            System.err.println("Erro ao criar o arquivo: " + e.getMessage());
+        }
+        System.out.println();
+        System.out.println(mensagemUsuario.getData() + " às " + mensagemUsuario.getHora() + " Arquivo " + conteudoMensagem.getNome() + " recebido de " + mensagemUsuario.getEmissor() + "!");
+        System.out.print(">>>");
+                        //(deliveryTag,               multiple);
+        //channel.basicAck(envelope.getDeliveryTag(), false);
+      }
+    };
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+    channel2.basicConsume(QUEUE_NAME + "Up", true, consumer2);
+    System.out.print( QUEUE_Send + " >>> ");
     String Inpt = sc.nextLine();
     while(!Inpt.equals("sair")){
       if(Inpt.substring(0,1).equals("@")){
@@ -83,9 +146,39 @@ public class Chat {
       else if(Inpt.substring(0,1).equals("#")){ //Enviar mensagens para o grupo
          QUEUE_Send = Inpt;
       }
-      else if(Inpt.substring(0,1).equals("!")){ //Comando de grupos
+      else if(Inpt.substring(0,1).equals("!")){
         textos = Inpt.split(" ");
-        if(textos[0].equals("!addGroup")){ //Adicionar Grupo
+        if(textos[0].equals("!upload")){
+          final String[] textosFinal = textos;
+          final String QueueNameAux = QUEUE_NAME;
+          final String QueueSendAux = QUEUE_Send;
+          Thread uploadThread = new Thread(() -> {
+          try {
+              Mensagem.MensagemUsuario.Builder builderMensagem = Mensagem.MensagemUsuario.newBuilder();
+          
+              Mensagem.Conteudo.Builder builderConteudo = Mensagem.Conteudo.newBuilder();
+              final Path source = Paths.get(textosFinal[1]);
+              String tipoMime = Files.probeContentType(source);
+              String[] textosSeparados = textosFinal[1].split("/");
+              builderConteudo.setNome(textosSeparados[textosSeparados.length - 1]);
+              builderConteudo.setCorpo(ByteString.readFrom(new FileInputStream(new File("/home/ubuntu/environment"+textosFinal[1]))));
+              builderConteudo.setTipo(tipoMime);
+              builderMensagem.setHora(horarioAtual());
+              builderMensagem.setData(dataAtual());
+              builderMensagem.setEmissor(QueueNameAux);
+              builderMensagem.setConteudo(builderConteudo);
+              System.out.println("\nEnviando " + textosFinal[1] + " para "  + QueueSendAux);
+              DecideEnvio(QueueSendAux+"Up", builderMensagem, channel2, QueueNameAux); // Usar o novo canal de upload
+              System.out.println("Arquivo " + textosFinal[1] + " foi enviado para "  + QueueSendAux);
+              System.out.print(QueueSendAux + " >>> ");
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+          });
+          uploadThread.start();
+          
+          }
+        else if(textos[0].equals("!addGroup")){ //Adicionar Grupo
           grupo = textos[1];
           channel.exchangeDeclare(grupo, "fanout");
           channel.queueBind(QUEUE_NAME, grupo , "");
@@ -108,23 +201,26 @@ public class Chat {
       }
       else{
         Mensagem.MensagemUsuario.Builder builderMensagem = Mensagem.MensagemUsuario.newBuilder();
+        
+        Mensagem.Conteudo.Builder builderConteudo = Mensagem.Conteudo.newBuilder();
+        builderConteudo.setCorpo(ByteString.copyFrom(Inpt,"UTF-8"));
+        
         builderMensagem.setHora(horarioAtual());
-        builderMensagem.setNome(QUEUE_NAME);
-        builderMensagem.setMensagem(Inpt);
-        if(QUEUE_Send.substring(0,1).equals("@")){
+        builderMensagem.setData(dataAtual());
+        builderMensagem.setEmissor(QUEUE_NAME);
+        builderMensagem.setConteudo(builderConteudo);
+        DecideEnvio(QUEUE_Send, builderMensagem, channel, QUEUE_NAME);
+        // if(QUEUE_Send.substring(0,1).equals("@")){
           
-          builderMensagem.setGrupo("");
-          Mensagem.MensagemUsuario mensagemUsuario = builderMensagem.build();
-          byte[] buffer = mensagemUsuario.toByteArray();
-          channel.basicPublish("", QUEUE_Send, null,  buffer); 
-        }
-        else{
+        //   builderMensagem.setGrupo("");
           
-          builderMensagem.setGrupo(QUEUE_Send);
-          Mensagem.MensagemUsuario mensagemUsuario = builderMensagem.build();
-          byte[] buffer = mensagemUsuario.toByteArray();
-          channel.basicPublish(QUEUE_Send.substring(1), QUEUE_NAME , null, buffer); 
-        }
+        //   channel.basicPublish("", QUEUE_Send, null,  buffer); 
+        // }
+        // else{
+          
+        //   builderMensagem.setGrupo(QUEUE_Send);
+        //   channel.basicPublish(QUEUE_Send.substring(1), QUEUE_NAME , null, buffer); 
+        // }
         
       }
       System.out.print(QUEUE_Send + " >>> ");
